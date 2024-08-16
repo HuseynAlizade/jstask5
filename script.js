56167f48c7651f6c240dbbfc6b42db757c41877c
HuseynAlizade <huseynalizade2010@gmail.com>
2024-08-16 23:46:16 +0400

    jstask5

---
+++ script.js
@@ -0,0 +1,51 @@
+//?Task1
+
+//const btn = document.getElementById("btn");
+//const text = document.getElementById("text");
+
+//function clicked() {
+//    text.innerText = "Hello World!";
+//}
+
+//function mouseOver() {
+ //   text.innerText = "Mouse Over!";
+//}
+
+//function mouseLeave() {
+ //   text.innerText = "Mouse Out!";
+//}
+
+//btn.addEventListener("mouseleave",mouseLeave);
+//btn.addEventListener("click",clicked);
+//btn.addEventListener("mouseover",mouseOver);
+
+//todoTask2
+
+//const btn = document.getElementById("btn");
+//const text = document.getElementById("text");
+
+//function clicked() {
+ //   text.innerText = "Hello World!";
+  //  text.style.color = "blue";
+//}
+
+//function mouseOver() {
+ ///   text.innerText = "Mouse Over!";
+ //   btn.style.padding ="20px";
+  //  btn.style.transition = "all 0.5s";
+  //  btn.style.background ="red";
+  //  btn.style.border ="none";
+///}
+
+//function mouseLeave() {
+ //   text.innerText = "Mouse Out!";
+   //   text.style.color = "black";
+    //   btn.style.padding ="10px";
+    //   btn.style.background ="green";
+   // btn.style.border ="1px solid gray";
+   // btn.borderRadius ="10px";
+//}
+
+//btn.addEventListener("mouseleave",mouseLeave);
+//btn.addEventListener("click",clicked);
+//btn.addEventListener("mouseover",mouseOver);
